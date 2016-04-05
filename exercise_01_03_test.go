a8be709b638a266cb439a36b5399f4894d3500ff
Xavier Noria <fxn@hashref.com>
2016-04-05 17:26:06 -0400

    Exercise 1.3

---
+++ exercise_01_03_test.go
@@ -0,0 +1,31 @@
+// I solved this by cargo-culting section 11.4 as suggested. Checked slice
+// literals in the official docs.
+//
+// echo3 is about twice times faster than echo1.
+//
+// By passing -benchmem I could also see that echo3 allocates half as much:
+// 2 allocs/op, versus 4 allocs/op of echo1.
+package exercise_01_03_test
+
+import (
+	"strings"
+	"testing"
+)
+
+var args = []string{"/usr/local/bin/go", "foo", "bar", "baz", "zoo", "woo"}
+
+func BenchmarkEcho1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var s, sep string
+		for j := 1; j < len(args); j++ {
+			s += sep + args[j]
+			sep = " "
+		}
+	}
+}
+
+func BenchmarkEcho3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.Join(args[1:], " ")
+	}
+}
