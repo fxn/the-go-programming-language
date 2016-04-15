fb8bf77ea3020004eee687d31b32a03add9cf2d8
Xavier Noria <fxn@hashref.com>
2016-04-15 11:40:03 +0200

    normalizes header in early exercises

--- ch01/exercise_01_03_test.go
+++ ch01/exercise_01_03_test.go
@@ -1,10 +1,17 @@
-// I solved this by cargo-culting section 11.4 as suggested. Checked slice
-// literals in the official docs.
-//
-// echo3 is about twice times faster than echo1.
-//
-// By passing -benchmem I could also see that echo3 allocates half as much:
-// 2 allocs/op, versus 4 allocs/op of echo1.
+/*
+  Exercise 1.3: Experiment to measure the difference in running time between our
+  potentially inefficient versions and the one that uses strings.Join. (Section
+  1.6 illustrates part of the time package, and Section 11.4 shows how to write
+  benchamrk tests for systematic performance evaluation.
+
+  Solution: I solved this by cargo-culting section 11.4 as suggested. Checked
+  slice literals in the official docs.
+
+  echo3 is about twice times faster than echo1.
+
+  By passing -benchmem I could also see that echo3 allocates half as much:
+  2 allocs/op, versus 4 allocs/op of echo1.
+*/
 package exercise_01_03_test
 
 import (
